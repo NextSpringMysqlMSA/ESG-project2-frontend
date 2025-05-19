766f861c8095c224ce9be3283d9ab331390eaad2
donghwan <kdhe9797@naver.com>
2025-05-19 16:41:36 +0900

    .

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,40 +1,15 @@
 import axios from 'axios'
 import {useAuthStore} from '@/stores/authStore'
 
-// API URL 동적 결정 함수
-const getApiBaseUrl = () => {
-  // 환경변수 확인
-  const configuredUrl = process.env.NEXT_PUBLIC_SPRING_API_URL
-
-  // 환경변수가 없거나 ${GATEWAY_ALB}와 같은 미치환 변수가 있는 경우
-  if (!configuredUrl || configuredUrl.includes('${') || configuredUrl === 'undefined') {
-    // 브라우저 환경인 경우 현재 호스트 기반으로 URL 생성
-    if (typeof window !== 'undefined') {
-      const hostname = window.location.hostname
-      const protocol = window.location.protocol
-      return `${protocol}//${hostname}`
-    }
-    // 서버사이드 렌더링 환경
-    return 'http://gateway-service'
-  }
-
-  return configuredUrl
-}
-
 // Axios 인스턴스 생성
 const api = axios.create({
-  baseURL: getApiBaseUrl(),
+  baseURL: process.env.NEXT_PUBLIC_SPRING_API_URL,
   withCredentials: true // 쿠키 사용할 경우 필요
 })
 
 // 요청 인터셉터
 api.interceptors.request.use(
   config => {
-    // 매 요청마다 baseURL 재확인 (SPA에서 필요할 경우)
-    if (typeof window !== 'undefined') {
-      config.baseURL = getApiBaseUrl()
-    }
-
     const getAuthHeader = useAuthStore.getState().getAuthorizationHeader
     const token = getAuthHeader?.()
 
@@ -43,7 +18,6 @@ api.interceptors.request.use(
     }
 
     console.log('📦 요청 헤더:', config.headers)
-    console.log('🔗 API URL:', config.baseURL)
 
     return config
   },
@@ -51,17 +25,17 @@ api.interceptors.request.use(
 )
 
 // 응답 인터셉터는 그대로 유지
-// api.interceptors.response.use(
-//   response => response,
-//   error => {
-//     if (error.response?.status === 401 || error.response?.status === 403) {
-//       if (typeof window !== 'undefined') {
-//         useAuthStore.getState().logout()
-//         window.location.href = '/login?error=unauthorized'
-//       }
-//     }
-//     return Promise.reject(error)
-//   }
-// )
+api.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response?.status === 401 || error.response?.status === 403) {
+      if (typeof window !== 'undefined') {
+        useAuthStore.getState().logout()
+        window.location.href = '/login?error=unauthorized'
+      }
+    }
+    return Promise.reject(error)
+  }
+)
 
 export default api
